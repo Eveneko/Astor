fec02e1b184128ae8f68c5636cd2df1670c52c97
Eveneko <huyubin0203@gmail.com>
2019-12-25 20:23:03 +0800

    update little little

--- apps/df_task/urls.py
+++ apps/df_task/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^logout/$', logout, name="logout"),
     url(r'^creat_task/$', creat_task, name="creat_task"),
     url(r'^task_record/$', task_record, name="task_record"),
+    url(r'^upload_data/$', upload_data, name="upload_data"),
+    url(r'^uploadFile/$', upload_file),  # 上传函数
 ]
--- apps/df_task/views.py
+++ apps/df_task/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
 from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from hashlib import sha1
 from apps.df_user import user_decorator
@@ -9,6 +9,8 @@ from django import forms
 from df_user.models import GoodsBrowser, UserInfo
 from df_order.models import OrderDetailInfo, OrderInfo
 from df_goods.models import GoodsInfo, TypeInfo
+
+import os
 
 
 @user_decorator.login
@@ -35,16 +37,14 @@ def logout(request):
 def creat_task(request):
     user_id = request.session['user_id']
     user = UserInfo.objects.get(id=request.session['user_id'])
-    algorithm_info = forms.ModelChoiceField(queryset=GoodsInfo.objects.all(),
-                                            required=True,
-                                            empty_label="Choose Algorithm",
-                                            initial="a+b")
+    algorithm_info = GoodsInfo.objects.all()
     context = {
         'title': 'creat_task',
         'uid': user_id,
         'user': user,
         'algorithm_info': algorithm_info,
     }
+    print(algorithm_info)
     return render(request, 'df_task/creat_task.html', context)
 
 
@@ -58,3 +58,27 @@ def task_record(request):
         'user': user,
     }
     return render(request, 'df_task/task_record.html', context)
+
+
+@user_decorator.login
+def upload_data(request):
+    user_id = request.session['user_id']
+    user = UserInfo.objects.get(id=request.session['user_id'])
+    context = {
+        'title': '用户中心',
+        'uid': user_id,
+        'user': user,
+    }
+    return render(request, 'df_task/upload_data.html', context)
+
+
+def upload_file(request):
+    if request.method == "POST":  # 请求方法为POST时，进行处理
+        File = request.FILES.get("file", None)  # 获取上传的文件，如果没有文件，则默认为None
+        if not File:
+            return HttpResponse("No files for upload!")
+        destination = open(os.path.join("../../Data/", File.name), 'wb+')  # 打开特定的文件进行二进制的写操作
+        for chunk in File.chunks():  # 分块写入文件
+            destination.write(chunk)
+        destination.close()
+        return HttpResponse("Upload over!")
