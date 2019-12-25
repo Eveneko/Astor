6ea245211661f59b2318dd9a825f55013d12b47e
Eveneko <huyubin0203@gmail.com>
2019-12-25 22:58:34 +0800

    user upload file interface

--- apps/df_task/urls.py
+++ apps/df_task/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     url(r'^creat_task/$', creat_task, name="creat_task"),
     url(r'^task_record/$', task_record, name="task_record"),
     url(r'^upload_data/$', upload_data, name="upload_data"),
-    url(r'^uploadFile/$', upload_file),  # 上传函数
+    url(r'^uploadFile/$', upload_file, name="upload_file"),  # 上传函数
 ]
