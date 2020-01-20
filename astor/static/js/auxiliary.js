516dbbd01065acc1884cb94cbdc268b147a89c90
Eveneko <huyubin0203@gmail.com>
2020-01-20 15:00:34 +0800

    清洗css和js

--- astor/static/js/auxiliary.js
+++
@@ -1,7 +0,0 @@
-var randomImage = ()=>{
-    return "/static/images/avatar/p"+Math.floor((Math.random() * 100)) + ".png";
-}
-
-var getImageFromID = (id)=>{
-    return "/static/images/avatar/p"+ id % 100 + ".png";
-}
